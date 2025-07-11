3e1eb8b78ff0f93074d3bcd546cf419c349bd145
mnky9800n <johnm.aiken@gmail.com>
2025-07-11 15:26:56 +0200

    created downloader.py

---
+++ downloader.py
@@ -0,0 +1,16 @@
+"""
+This downloads all data from ZENODO that is in the IODP community. THe data is 
+downloaded as is and is not parsed.
+
+You will need a Zenodo API key which can be created via the Zenodo Applications
+link when you login. Once created you should export the key like so:
+
+$export ZENODO_API_KEY = '<your key that you copied from zenodo>'
+"""
+
+
+# IMPORTS
+
+ZENODO_API_KEY = # you should createa
+
+# 
